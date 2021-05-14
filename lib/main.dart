a486bfa53e51b52b628c497171944a96c6a7e1bf
sahil-santosh <sahilbudania@gmail.com>
2021-05-14 15:33:30 +0530

    day3

--- lib/main.dart
+++ lib/main.dart
@@ -1,26 +1,30 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_catalog/pages/home_page.dart';
 import 'package:flutter_catalog/pages/login_page.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 void main() {
   runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       // home: HomePage(),          // you can define HomePage() here
-      themeMode: ThemeMode.light,   // akele ye likhne se kuch nhi hoga hume darkTheme bhi dena padega then it works
-      theme: ThemeData(primaryColor: Colors.deepPurple),
+      themeMode: ThemeMode
+          .light, // akele ye likhne se kuch nhi hoga hume darkTheme bhi dena padega then it works
+      theme: ThemeData(
+        primaryColor: Colors.deepPurple,
+        fontFamily: GoogleFonts.lato().fontFamily,
+      ),
       darkTheme: ThemeData(brightness: Brightness.dark),
-      initialRoute: "/login",//when we run app by deafult we saw the HomePage() if WE set initial route ,now we saw the login page when app run
+      initialRoute: "/login", //when we run app by deafult we saw the HomePage() if WE set initial route ,now we saw the login page when app run
       routes: {
-        "/": (context)=> HomePage(),  //or you can define HomePage() here otherwise this gave you error
+        "/": (context) => HomePage(), //or you can define HomePage() here otherwise this gave you error
         "/login": (context) => LoginPage(),
-        },  
+      },
     );
   }
 }
--- lib/pages/login_page.dart
+++ lib/pages/login_page.dart
@@ -4,9 +4,42 @@ class LoginPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Material(
-      child: Center(
-        child: Text("login",style: TextStyle(fontSize: 20),textScaleFactor: 2.0,),
-        
+      color: Colors.white,
+      child: Column(
+        children: [
+          Image.asset(
+            "assets/images/login_image.png",
+            fit: BoxFit.cover,
+          ),
+          SizedBox(height: 20,),
+          Text("Welcome",style: TextStyle(fontWeight: FontWeight.bold,fontSize: 22,),),
+          SizedBox(height: 10,),
+          Padding(
+            padding: EdgeInsets.symmetric(vertical: 16,horizontal: 32),
+            child: Column(
+              children: [
+                TextFormField(
+                  decoration: InputDecoration(
+                    hintText: "Enter username",
+                    labelText: "Username",
+                  ),
+                ),
+                TextFormField(
+                  obscureText: true,
+                  decoration: InputDecoration(
+                    hintText: "Enter password",
+                    labelText: "Password",
+                  ),
+                ),
+                SizedBox(height: 20,),
+                ElevatedButton(
+                  child: Text("Login"),
+                  onPressed: (){print("hi login");},
+                ),
+              ],
+            ),
+          ),
+        ],
       ),
     );
   }
